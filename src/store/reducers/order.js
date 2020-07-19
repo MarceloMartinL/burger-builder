43240966409164ff0c5b657f757cb30e891c2469
Marcelo <marcelo18a@msn.com>
2020-07-18 20:00:38 -0400

    fixes

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -8,11 +8,11 @@ const initialState = {
 };
 
 const purchaseInit = (state, action) => {
-	return updateObject(state, {purchased: false});
+	return updateObject(state, { purchased: false });
 };
 
 const purchaseBurgerStart = (state, action) => {
-	return updateObject(state, {loading: true});
+	return updateObject(state, { loading: true });
 };
 
 const purchaseBurgerSuccess = (state, action) => {
@@ -25,11 +25,11 @@ const purchaseBurgerSuccess = (state, action) => {
 };
 
 const purchaseBurgerFail = (state, action) => {
-	return updateObject(state, {loading: false});
+	return updateObject(state, { loading: false });
 };
 
 const fetchOrdersStart = (state, action) => {
-	return updateObject(state, {loading: true});
+	return updateObject(state, { loading: true });
 };
 
 const fetchOrdersSuccess = (state, action) => {
